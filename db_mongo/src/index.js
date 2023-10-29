0acfc0578046d617a9f9abc99ad38707d6e149da
Deepak Sharma <stdeepak22@gmail.com>
2023-10-30 00:53:18 +0530

    removed the .md file from db_mongo > seed_init

--- db_mongo/src/index.js
+++ db_mongo/src/index.js
@@ -1,13 +1,12 @@
-import { ConnectDB, DisconnectDB } from "./db/mongooseConnection.js";
-import { Restaurant, Review, User } from "./db/schema/index.js";
+import { ConnectDB, DisconnectDB, ObjectId, getFiredQuries } from "./db/mongooseConnection.js";
+import { User, Restaurant, Review } from "./db/schema/index.js";
 
 await ConnectDB();
-
-console.log('user count is ', await User.count());
-console.log('review count is ', await Review.count());
-console.log('restaurant count is ', await Restaurant.count());
+await TestLogic();
+await DisconnectDB();
 
 
-console.log('review with rating 5 Star ', await Review.count({ rating: 5 }));
 
-DisconnectDB();
+async function TestLogic() {
+    // you can write your mongo code here and can quickly test it using "node src/index.js"
+}
