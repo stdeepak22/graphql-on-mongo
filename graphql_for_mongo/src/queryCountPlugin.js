c2236f4c36846df31076c19850b99c619d0acab8
Deepak Sharma <stdeepak22@gmail.com>
2023-10-29 23:17:55 +0530

    added sibling mongodb local package to package.json file also ignore the introspection query from query count plugin

--- graphql_for_mongo/src/queryCountPlugin.js
+++ graphql_for_mongo/src/queryCountPlugin.js
@@ -1,12 +1,19 @@
 import { clearPreviousFiredQueries, getFiredQueries } from "@stdeepak22/db_mongo";
 
+
+/**
+ * To count the MongoDB queries for each GraphQL query, exclude the "IntrospectionQuery".
+ * IntrospectionQuery = query that apollo sendbox execute to get meta data of types, query and mutation.
+ */
 export const queryCountPlugin = () => {
     return {
-        async requestDidStart() {
-            clearPreviousFiredQueries();
-            return {
-                willSendResponse: async () => {
-                    console.log('\t', new Date().toISOString(), 'fired', getFiredQueries().length, 'quries.');
+        async requestDidStart({ request }) {
+            if (request.operationName !== 'IntrospectionQuery') {
+                clearPreviousFiredQueries();
+                return {
+                    willSendResponse: async () => {
+                        console.log('\t', new Date(), 'Executed', getFiredQueries().length, 'MongoDB queries.');
+                    }
                 }
             }
         }
